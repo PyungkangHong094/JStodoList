ced4c7e321589ecf79a783ecc91d5f2ed3ec6e9a
PKHong <pyungkang1004@gmail.com>
2021-07-18 14:36:02 +0900

    form event submission

--- app.js
+++ app.js
@@ -1,72 +1,32 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginForm = document.querySelector("#login-form");
 
-// title.innerText="got you!";
+//쿼리셀렉터를 사용하면 (<#사용한id> <넣은것들 ex button, input>)
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-//console.log(title.id); //title
+//기능을 넣어준것이다 (이름은 자유)
+// function onLoginBtnClick(){
+//     const value = loginInput.value;
+//     if(value === ""){
+//         alert("Please write your name");
+//     }
+//     else if(value.length > 15){
+//         alert("Your name is too long.");
+//     }
+//     else if(value.length < 5){
+//         alert("Your name is too short.");
+//     }
+//     console.log(loginInput.value);
+//     console.log("click!!");
+// }
 
-
-//querySelector 는 element를 CSS방식으로 검색할수 있다  (.<CSS> something )
-//const title = document.querySelector(".hello h1"); 
-
-//h1 html emelemet mdn (머신 디벨러퍼 머신)
-
-function handleTitleClick() {
-    const clickClass = "actived";
-    //css랑 이름이 같아야한다
-    //h1.className === clickClass
-    if(h1.classList.contains(clickClass)){
-        // h1.className = "";
-        h1.classList.remove(clickClass);
-    }
-    else{
-        // h1.className = clickClass;
-        h1.classList.add(clickClass);
-    }
+function onLoginSubmit(event){
+    event.preventDefault();    
+    console.log(loginInput.value);
 }
 
-h1.addEventListener("click", handleTitleClick);
+//클릭이라는 이벤트를 넣어줌
+// loginButton.addEventListener("click",onLoginBtnClick);
 
-//클릭을 했을때 글자가 바뀌는 현상
-// function handleTitleClick_test() {
-//     const currentColor = h1.style.color;
-//     let  newcolor;
-//     console.log(h1.style.color);
-//     // h1.style.color ="blue";
-//     console.log(h1.style.color);
-    
-//     if(currentColor === "blue"){
-//         newcolor = "tomato";
-//     }
-//     else{
-//         newcolor="blue";
-//     }
-//     h1.style.color = newcolor;
-// }
-// h1.addEventListener("click", handleTitleClick_test);
-
-// function handleMouseEnter() {
-//     console.log("mouse is here!");    
-// };
-// //사이즈 바뀌면 바뀌는거 
-// function handlewindowResize(){
-//     document.body.style.backgroundColor = "tomato";
-// }
-// //카피하면 보이는거 
-// function handlewindowCopy(){
-//     alert("Copier!")
-// }
-
-// function handleWindowoffline(){
-//     alert("No Wifi");
-// }
-
-// function handlewindowonline(){
-//     alert("You have Wife Set up");
-// }
-// title.onclick = handleTitleClick;
-// title.addEventListener("mouseenter",handleMouseEnter);
-
-// window.addEventListener("resize", handlewindowResize); //사이즈 바뀌면 바뀌는거 
-// window.addEventListener("copy", handlewindowCopy); //카피하면 보이는거 
-// window.addEventListener("offline", handleWindowoffline); // 와이파이가 있으면 
-// window.addEventListener("online", handlewindowonline); //와이파이가있으면 보여지는거 
+//summit evern
+loginForm.addEventListener("submit", onLoginSubmit);
---
+++ testfile/test01.js
@@ -0,0 +1,76 @@
+const h1 = document.querySelector("div.hello:first-child h1");
+
+// title.innerText="got you!";
+
+//console.log(title.id); //title
+
+
+//querySelector 는 element를 CSS방식으로 검색할수 있다  (.<CSS> something )
+//const title = document.querySelector(".hello h1"); 
+
+//h1 html emelemet mdn (머신 디벨러퍼 머신)
+
+function handleTitleClick() {
+    const clickClass = "actived";
+    //css랑 이름이 같아야한다
+    //h1.className === clickClass
+    if(h1.classList.contains(clickClass)){
+        // h1.className = "";
+        h1.classList.remove(clickClass);
+    }
+    else{
+        // h1.className = clickClass;
+        h1.classList.add(clickClass);
+    }
+    //더 간단한 코드는 
+    //toggle은 h1의 classlist에 actived class 가 이미있는지 확인해서  만약있다면 ,toogle이 클릭을 제거해준다
+    //만약 h1 classlist에 clicked가 존재하지 않는다면 toggle은 clicked를 classlist에 추가를 해주는거다 
+    h1.classList.toggle("actived");
+}
+
+h1.addEventListener("click", handleTitleClick);
+
+//클릭을 했을때 글자가 바뀌는 현상
+// function handleTitleClick_test() {
+//     const currentColor = h1.style.color;
+//     let  newcolor;
+//     console.log(h1.style.color);
+//     // h1.style.color ="blue";
+//     console.log(h1.style.color);
+    
+//     if(currentColor === "blue"){
+//         newcolor = "tomato";
+//     }
+//     else{
+//         newcolor="blue";
+//     }
+//     h1.style.color = newcolor;
+// }
+// h1.addEventListener("click", handleTitleClick_test);
+
+// function handleMouseEnter() {
+//     console.log("mouse is here!");    
+// };
+// //사이즈 바뀌면 바뀌는거 
+// function handlewindowResize(){
+//     document.body.style.backgroundColor = "tomato";
+// }
+// //카피하면 보이는거 
+// function handlewindowCopy(){
+//     alert("Copier!")
+// }
+
+// function handleWindowoffline(){
+//     alert("No Wifi");
+// }
+
+// function handlewindowonline(){
+//     alert("You have Wife Set up");
+// }
+// title.onclick = handleTitleClick;
+// title.addEventListener("mouseenter",handleMouseEnter);
+
+// window.addEventListener("resize", handlewindowResize); //사이즈 바뀌면 바뀌는거 
+// window.addEventListener("copy", handlewindowCopy); //카피하면 보이는거 
+// window.addEventListener("offline", handleWindowoffline); // 와이파이가 있으면 
+// window.addEventListener("online", handlewindowonline); //와이파이가있으면 보여지는거 
